4ea155ebd252c846ece98f231128fc7cf5458681
kaylahoo <790970276@qq.com>
2023-06-05 23:15:48 +0800

    paris

--- src/models.py
+++ src/models.py
@@ -2,7 +2,7 @@ import os
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from .networks import InpaintGenerator, InpaintGenerator1,EdgeGenerator, Discriminator
+from .networks import InpaintGenerator,EdgeGenerator, Discriminator
 from .loss import AdversarialLoss, PerceptualLoss, StyleLoss
 
 
